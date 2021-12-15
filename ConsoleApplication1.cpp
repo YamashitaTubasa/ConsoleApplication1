a48322693461fc839769a36cb428c1546304041b
yamashitatubasa <k021g1247@g.neec.ac.jp>
2021-12-15 15:55:27 +0900

    Hello World

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -5,10 +5,10 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World\n";
 
     std::cout << "ブランチを追加\n";
-
+    
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
